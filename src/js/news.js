6e6f2f9de1185308eea69dc8f84ed1d87ef75411
JoseVelizFTF <jose.veliz@fulltimeforce.com>
2020-08-20 23:49:59 -0500

    news style added

--- src/js/news.js
+++ src/js/news.js
@@ -1,41 +1,275 @@
-
-(async function(){
-    const location = window.location.href;
-    const q = location.split("?");
-    const topic = q.length > 1 ? q[1] : "world";
-    config = {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": `${NEWS_API_HOST}`,
-            "x-rapidapi-key": `${NEWS_API_KEY}`
-        }
-    };
-    const data = await get(SEARCH_FREE(`page_size=10&lang=es&media=True&q=${topic}`, config));
-    render(data);
+var mockData = {
+  status: "ok",
+  total_hits: 562,
+  page: 1,
+  total_pages: 57,
+  page_size: 10,
+  articles: [
+    {
+      summary:
+        "Skip to main content World + 10 more COVID-19 pandemic│DG ECHO Daily Map│20/08/2020 Download map (PDF | 1023.11 KB) World + 11 more Guía para la creación de mecanismos derivación seguros para personas en situación de migración y personas refugiadas World + 14 more Las voces de los niños y niñas en tiempos de COVID-19: Activismo permanente de los niños y niñas a pesar de los desafíos personales World Children and young people's voices in the time of the COVID-",
+      country: "NA",
+      author:
+        "European Commission's Directorate-General for European Civil Protection and Humanitarian Aid Operations",
+      link:
+        "https://reliefweb.int/map/world/covid-19-pandemic-dg-echo-daily-map-20082020",
+      language: "es",
+      media:
+        "https://reliefweb.int/sites/reliefweb.int/files/styles/attachment-large/public/resources-pdf-previews/1532084-20200820_Coronavirus_EEAS_world.png?itok=6GbY6jc9",
+      title: "World: COVID-19 pandemic│DG ECHO Daily Map│20/08/2020",
+      media_content: null,
+      clean_url: "reliefweb.int",
+      rights:
+        "You should respect the intellectual property rights of the original source. Please contact the source directly if you want to re-use their content.",
+      rank: "2600",
+      topic: "NA",
+      published_date: "2020-08-20 17:19:53",
+      _id: "b26beb1f2a6bd4570ec4d3e14b64b7df",
+      _score: 11.1994,
+    },
+    {
+      summary:
+        "Minecraft ha anunciado un nuevo DLC temático de Jurassic World que permitirá a los jugadores administrar su propio parque temático de dinosaurios en el juego. El DLC Jurassic World permite a los jugadores crear y entrenar dinosaurios, construir exhibiciones y administrar las operaciones de un divertido parque que está plagado de dinosaurios. Además, el contenido incluye el parque Jurassic World con ubicaciones de las películas, 21 máscaras y más de 60 dinosaurios. Básicamente, se trata de un simulador de parques basado en Jurassic World que se puede jugar en Minecraft.",
+      country: "US",
+      author: "Ign",
+      link:
+        "https://es.ign.com/minecraft-pc/166228/news/minecraft-anunciado-un-dlc-de-jurassic-world",
+      language: "es",
+      media:
+        "https://sm.ign.com/t/ign_es/news/j/jurassic-w/jurassic-world-dlc-released-in-minecraft_uv8b.1200.jpg",
+      title: "Minecraft: Anunciado un DLC de Jurassic World",
+      media_content: null,
+      clean_url: "ign.com",
+      rights: "ign.com",
+      rank: "721",
+      topic: "NA",
+      published_date: "2020-08-19 15:40:23",
+      _id: "6b4a7a6df00dac80846a6edf14289a14",
+      _score: 10.837847,
+    },
+    {
+      summary:
+        '"Jurassic World: Dominion" está siendo el ejemplo de que el pandemia por coronavirus aún se puede continuar filmando de forma segura EE.UU.— A través de redes sociales se ha dado a conocer que el rodaje de la película "Jurassic World: Dominion" se reanudó con todos los protocolos sanitarios. Por su parte, Bryce Dallas Howard, la […] More The post Muestran cómo se filma "Jurassic World: Dominion" en plena pandemia appeared first on El Diario de Yucatán.',
+      country: "MX",
+      author: "Ali Paulino",
+      link:
+        "https://www.yucatan.com.mx/espectaculos/muestran-como-se-filma-jurassic-world-dominion-en-plena-pandemia",
+      language: "es",
+      media: null,
+      title:
+        'Muestran cómo se filma "Jurassic World: Dominion" en plena pandemia',
+      media_content: null,
+      clean_url: "yucatan.com.mx",
+      rights: "yucatan.com.mx",
+      rank: "31408",
+      topic: "news",
+      published_date: "2020-08-15 21:39:44",
+      _id: "2b51b9c603608827680366a95dd49da4",
+      _score: 10.534746,
+    },
+    {
+      summary:
+        "Revelan detalles de la filmación de Jurassic World Redacción  La nueva película de ‘Jurassic World: Dominion', ha reanudado su producción después de haber sido suspendida debido a la pandemia mundial, y como adelanto han sido compartido nuevas imágenes del set. Las nuevas fotos fueron compartidas como parte de un artículo de The New York Times, llevando a […] La entrada Así filman ‘Jurassic World: Dominion' se publicó primero en AM Querétaro.",
+      country: "NA",
+      author: "Estefany González",
+      link:
+        "https://amqueretaro.com/viral/2020/08/15/asi-filman-jurassic-world-dominion/",
+      language: "es",
+      media: null,
+      title: "Así filman ‘Jurassic World: Dominion'",
+      media_content: null,
+      clean_url: "amqueretaro.com",
+      rights: "amqueretaro.com",
+      rank: "219969",
+      topic: "news",
+      published_date: "2020-08-15 23:18:21",
+      _id: "131b194454d6822193c36fef16c66a8a",
+      _score: 10.304507,
+    },
+    {
+      summary:
+        "Este 2020, Ecuador alcanzó 42 nominaciones en distintas categorías en la vigésima séptima edición en Sudamérica de los World Travel Awards, considerados los 'Oscar del Turismo'.",
+      country: "EC",
+      author: "Redacción Elcomercio.com",
+      link:
+        "http://www.elcomercio.com/tendencias/ecuador-nominaciones-world-travel-awards.html",
+      language: "es",
+      media: null,
+      title:
+        "Ecuador obtiene 42 nominaciones en los World Travel Awards; las votaciones son hasta el 1 de septiembre",
+      media_content: null,
+      clean_url: "elcomercio.com",
+      rights: "elcomercio.com",
+      rank: "10992",
+      topic: "news",
+      published_date: "2020-08-17 17:18:00",
+      _id: "743cd8e3c4e7beea7aca53df7a7b0c74",
+      _score: 10.174766,
+    },
+    {
+      summary:
+        "Los festejos por el décimo aniversario de 'Scott Pilgrim vs. the World' continúan. La entrada ¡Checa la increíble reedición en vinilo del soundtrack de ‘Scott Pilgrim vs. the World’! aparece primero en LifeBoxset.",
+      country: "NA",
+      author: "Marisol Martínez",
+      link:
+        "https://www.lifeboxset.com/nota/226796/checa-la-increible-reedicion-en-vinilo-del-sountrack-de-scott-pilgrim-vs-the-world/",
+      language: "es",
+      media: null,
+      title:
+        "¡Checa la increíble reedición en vinilo del soundtrack de ‘Scott Pilgrim vs. the World'!",
+      media_content: null,
+      clean_url: "lifeboxset.com",
+      rights: "lifeboxset.com",
+      rank: "394743",
+      topic: "music",
+      published_date: "2020-08-17 16:35:18",
+      _id: "d7af050432dd7edb4a2e0294dc8a6666",
+      _score: 10.102494,
+    },
+    {
+      summary:
+        "Desde Nintendo han anunciado hoy que mañana 18 de agosto se emitirá un nuevo Indie World a las 18:00 (hora española peninsular) / 09:00 PT / 00:00 ET con información de los próximos juegos Indies que llegarán a Nintendo Switch. La presentación podrá verse aquí o aquí.",
+      country: "RU",
+      author: "news+feed@crunchyroll.com",
+      link:
+        "https://www.crunchyroll.com/anime-news/2020/08/17-1/nintendo-celebrar-maana-una-nueva-presentacin-indie-world",
+      language: "es",
+      media:
+        "https://img1.ak.crunchyroll.com/i/spire4/31974be48c8add703bd9fd40c1d388821597700406_thumb.jpg",
+      title: "Nintendo celebrará mañana una nueva presentación Indie World",
+      media_content: null,
+      clean_url: "crunchyroll.com",
+      rights: "Copyright © 2020 Ellation Inc. All rights reserved.",
+      rank: "3035",
+      topic: "entertainment",
+      published_date: "2020-08-17 21:40:16",
+      _id: "e23d82df7ef76041807dfc1eff9dcd66",
+      _score: 10.094182,
+    },
+    {
+      summary:
+        "Estos son los 9 anuncios más importante del Nintendo Indie World Showcase.",
+      country: "NA",
+      author: "La Verdad",
+      link:
+        "https://laverdadnoticias.com/tecnologia/Anuncios-mas-IMPORTANTES-del-Nintendo-Indie-World-Showcase-20200818-0163.html",
+      language: "es",
+      media:
+        "https://laverdadnoticias.com/__export/1597777066035/sites/laverdad/img/2020/08/18/ihndie_world_nintendo.jpg_1187572215.jpg",
+      title: "Anuncios más IMPORTANTES del Nintendo Indie World Showcase",
+      media_content:
+        "https://laverdadnoticias.com/__export/1597777066035/sites/laverdad/img/2020/08/18/ihndie_world_nintendo.jpg_1187572215.jpg",
+      clean_url: "laverdadnoticias.com",
+      rights: "laverdadnoticias.com",
+      rank: "62823",
+      topic: "news",
+      published_date: "2020-08-18 18:57:47",
+      _id: "5b1b627fffeb32d184531b26b68a2266",
+      _score: 10.062418,
+    },
+    {
+      summary:
+        "la voz a coruña 14/08/2020 16:57 h La ciudad de A Coruña ha sido propuesta como candidata a ser la «ciudad de la felicidad» en los premios World Happiness Awards. Estos galardones son una iniciativa de World Happiness Fest y buscan dar reconocimiento a acciones, ideas y proyectos «liderados desde el corazón», explican los responsables. Además se puede nominar durante todo el año y los organizadores anuncian a los ganadores de los premios cuatro veces al año en las diferentes categorías. Se realizan siempre el día 20 en los meses de septiembre, diciembre, marzo y junio.",
+      country: "ES",
+      author: "la voz",
+      link:
+        "https://www.lavozdegalicia.es/noticia/coruna/coruna/2020/08/14/coruna-opta-ciudad-felicidad-premios-world-happiness-award/00031597415415420594424.htm",
+      language: "es",
+      media:
+        "https://www.lavozdegalicia.es/default/2020/08/14/00121597417201975805122/Foto/HG10C3F1_16591.jpg",
+      title:
+        "A Coruña opta a ser la «ciudad de la felicidad» en los premios World Happiness Award",
+      media_content:
+        "https://i.avoz.es/sc/960x/default/2020/08/14/00121597417201975805122/Foto/HG10C3F1_16591.jpg",
+      clean_url: "lavozdegalicia.es",
+      rights: "LA VOZ DE GALICIA S.A.",
+      rank: "6753",
+      topic: "NA",
+      published_date: "2020-08-14 14:57:00",
+      _id: "b9e3201731572b067cbef075f8886c4f",
+      _score: 10.03619,
+    },
+    {
+      summary:
+        'El complejo deportivo "Soccer World", colindante con el pabellón de Perchera-La Braña y construido en el año 2011, centró ayer el debate en una reunión de la Asociación de Vecinos "Santiago" de Nuevo Gijón, La Braña y Perchera. Los asistentes...',
+      country: "NA",
+      author: "La Nueva España",
+      link:
+        "https://www.lne.es/gijon/2020/08/18/nuevo-gijon-pide-ayuntamiento-gestion/2672196.html?utm_source=rss",
+      language: "es",
+      media: null,
+      title:
+        'Nuevo Gijón pide al Ayuntamiento una gestión comunitaria del "Soccer World"',
+      media_content: null,
+      clean_url: "lne.es",
+      rights: "Copyright La Nueva España",
+      rank: "13209",
+      topic: "NA",
+      published_date: "2020-08-18 01:04:54",
+      _id: "db87b73202527b7b37c3037be25c8d22",
+      _score: 10.030937,
+    },
+  ],
+  user_input: {
+    q: "world",
+    search_in: "title_summary_es",
+    lang: "es",
+    ranked_only: "True",
+    sort_by: "relevancy",
+    from: "2020-08-14 00:00:00",
+    page: 1,
+    size: 10,
+    media: "True",
+  },
+};
+(async function () {
+  const location = window.location.href;
+  const q = location.split("?");
+  const topic = q.length > 1 ? q[1] : "world";
+  config = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": `${NEWS_API_HOST}`,
+      "x-rapidapi-key": `${NEWS_API_KEY}`,
+    },
+  };
+  //   const data = await get(
+  //     SEARCH_FREE(`page_size=10&lang=es&media=True&q=${topic}`, config)
+  //   );
+  const data = mockData;
+  render(data);
 })();
 
-
-function render(data){
-    var wrapper = document.getElementById('Contenido');
-    var html = data.articles.map((item,index) => {
-        return(`
-        <div class="media col-xs-12">
-            <div class="row">
-                <div class="col-sm-4">
-                    <img class="mr-3 img-fluid" src="${item.media}" alt="${item.author}"/>    
+function render(data) {
+  console.log("data", data.articles);
+  var wrapper = document.getElementById("Contenido");
+  var html = data.articles
+    .map((item, index) => {
+      return `
+        <div class="new">
+            <div class="new__image_container">
+                <img class="new__image" src="${item.media}" alt="${
+        item.author
+      }"/>
+            </div>
+            <div>
+                <div class="new__title" class="mt-0">
+                    ${item.title}
                 </div>
-                <div class="col-sm-8">
-                    <div class="media-body">
-                        <h5 class="mt-0">${item.title}</h5>
-                        ${item.summary}
-                    </div>
+                <div class="new__metadata">
+                    <span>${item.author.substr(0, 15)}</span>
+                    <span>
+                        ${new Date(item.published_date).toLocaleString()}
+                    </span>
                 </div>
-            </div> 
+                <div class="new__content">${item.summary.substr(
+                  0,
+                  300
+                )}...<a href="#">Ver mas</a></div>
+            </div>
         </div>
-        `);
-    }).join(" ");
-    wrapper.innerHTML = html;
+      `;
+    })
+    .join(" ");
+  wrapper.innerHTML = html;
 }
-
-
-
